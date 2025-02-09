2e4a142d3ce3221e73ec1274c3acf85fa8e29455
Muhammet Atilla <muhammetcanaltan14@gmail.com>
2025-02-09 17:11:01 +0300

    Tool

---
+++ setup.sh
@@ -0,0 +1,41 @@
+figlet " Termux-Setup "
+Github: https://github.com/babarich456/Termux-Setup.git
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+red='\e[1;31m'
+default='\e[0m'
+yellow='\e[0;33m'
+orange='\e[38;5;166m'
+green='\033[92m'
+
+clear
+sleep 1.5
+echo -e "$default"
+apt update
+apt upgrade
+termux-setup-storage
+pkg install git
+pkg install wget
+pkg install python
+pkg install python2
+pkg install python3
+pkg install php
+pkg install perl
+pkg install nano
+pkg install vim
+pkg install cat
+pkg install pip
+pkg install pip2
+pkg install pip3
+pip install wordlist
+pkg install nmap
+pkg install hydra
+pkg install openssl
+pkg install ruby
+apt install nodejs
+cd
+cd
+cd Termux-Setup
+python completed.py

5444191a5e363ae25dfc316262fff812d0a7c508
Muhammet Atilla <muhammetcanaltan14@gmail.com>
2025-02-15 19:38:43 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,16 +1,8 @@
-g="\033[1;32m"
-r="\033[1;31m"
-b="\033[1;34m"
-w="\033[0m"
-red='\e[1;31m'
-default='\e[0m'
 yellow='\e[0;33m'
-orange='\e[38;5;166m'
-green='\033[92m'
 
 clear
 sleep 1.5
-echo -e "$default"
+echo -e "$yellow" Entered the System.
 bash started.sh
 apt update
 apt upgrade
